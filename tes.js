0e8aa6de61db3c4e433c7f2ce7b7f8bdf367bc7a
RedenHilal <hilalrahmatullah11@gmail.com>
2025-01-25 07:36:00 +0700

    yes

--- tes.js
+++
@@ -1,8 +0,0 @@
-import path from 'path'
-import { fileURLToPath } from 'url';
-import { dirname } from 'path';
-
-const filename = fileURLToPath(import.meta.url)
-const dirpath = dirname(filename)
-
-console.log(dirpath)
