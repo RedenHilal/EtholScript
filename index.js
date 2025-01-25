21ccfe64a4aba65ae30861a21be3f9f8dd518d14
RedenHilal <hilalrahmatullah11@gmail.com>
2025-01-25 07:29:39 +0700

    automically search for binary file, currently for windows and linux

--- index.js
+++ index.js
@@ -4,7 +4,11 @@ import dotenv from "dotenv";
 import { spawn } from "node:child_process";
 import { fileURLToPath } from 'url';
 import { dirname } from 'path';
-import { dir } from "node:console";
+import os from 'os'
+import { resolve } from "node:path";
+import { exit } from "node:process";
+
+const envOs = os.platform === "win32"? "windows":"linux"
 
 const filename = fileURLToPath(import.meta.url)
 const dirpath = dirname(filename)
@@ -26,6 +30,32 @@ const flags  = {
 }
 
 const arrayMatkul = (await readMatkulJSON()).map((el)=>el.matakuliah.nama)
+
+async function getBinaryPath(){
+    return new Promise((resolve,reject)=>{
+        let command;
+        let path = ""
+        if(envOs === "windows"){
+            command = spawn("where", ["chrome.exe"])
+        }
+        else {
+            command = spawn("which", ["google-chrome-stable"])
+        }
+        command.stdout.on('data',data =>{
+            path += data.toString().trim()
+        })
+        command.stderr.on('data',data=>{
+            console.log("Error finding binary file\nExiting Now...")
+        })
+        command.on("close",code=>{
+            if(code === 0){
+                resolve(path)
+            } else{
+                reject(0)
+            }
+        })
+    })
+}
 
 async function readMatkulJSON (){
         const file = await fs.readFileSync(`${dirpath}/matkulDetail.json`, 'utf8')
@@ -54,17 +84,14 @@ async function checkNew(output){
         })
     })
 }
-async function RenewMapel(page){
+async function RenewMapel(response){
     try{
-        const requests = await page.on('response',async (response)=>{
-            const responUrl = await response.request().url()
-            const urlRegex = /[?&]tahun=\d+&semester=\d+/
-            if(urlRegex.test(responUrl)){
-                const body = await response.json()
-                await fs.writeFileSync(`${dirpath}/matkulDetail.json`, JSON.stringify(body,null,2))
-                
-            }
-        })
+        const responUrl = await response.request().url()
+        const urlRegex = /[?&]tahun=\d+&semester=\d+/
+        if(urlRegex.test(responUrl)){
+            const body = await response.json()
+            await fs.writeFileSync(`${dirpath}/matkulDetail.json`, JSON.stringify(body,null,2))
+        }
         console.log("Renewed Successfully")
         await importMatkul()
         
@@ -119,6 +146,52 @@ async function listMatkul(){
         }
     }
 }
+async function checkNewAssignment(response){
+    const requestUrl = await response.request().url()
+    if(requestUrl === "https://ethol.pens.ac.id/api/tugas/tugas-terakhir-mahasiswa"){
+        try{
+            const responseBody = await response.json();
+           
+                
+                const status = await checkNew(responseBody);
+                fs.writeFileSync(`${dirpath}/output.json`, JSON.stringify(responseBody, null, 2))
+                switch (status){
+                    case 0:
+                        console.log("No New Assignment");
+                        if(flags.assignmentRaw){
+                            console.log(responseBody)
+                            break;
+                        }
+                        const file0 = await fs.readFileSync(`${dirpath}/output.json`, 'utf8')
+                        const outputjson0 = JSON.parse(file0)
+                        for(let i = 0; i < outputjson0.length;i++){
+                            console.log(`${i+1}. ${outputjson0[i].title}\n   Deadline:${outputjson0[i].deadline}`)
+                        }
+                        break;
+                    case 1:
+                        console.log("New Assignment Avaible");
+                        if(flags.assignmentRaw){
+                            console.log(responseBody)
+                            break;
+                        }
+                        const file1 = await fs.readFileSync(`${dirpath}/output.json`, 'utf8')
+                        const outputjson1 = JSON.parse(file1)
+                        for(let i = 0; i < outputjson1.length;i++){
+                            console.log(`${i+1}. ${outputjson1[i].title}\n   Deadline:${outputjson1[i].deadline}`)
+                        }
+                        break;
+                    case 2:
+                        console.log("Error on checking file");
+                        break;
+                
+                //console.log(responseBody);
+            }
+        }
+        catch(err){
+        
+        }
+    }   
+}
 
 async function EtholHook(){
 
@@ -128,69 +201,30 @@ async function EtholHook(){
     }
     if (flags.matkulList) listMatkul();
 
+    const path = await getBinaryPath();
+    if(!path){
+        console.log("exiting now...");
+        exit(1);
+    }
     
     const browser = await puppeteer.launch({
-        executablePath:"/usr/bin/google-chrome-stable",
+        executablePath:path,
         headless:flags.debug? false:true
     });
     try{
         const page = await browser.newPage();
 
         await page.goto("https://ethol.pens.ac.id/");
-        if (flags.checkAssignment){
+        if (flags.checkAssignment || flags.renewMapel){
             page.on('response', async response => {
         
-                const responseStatus = response.status();
-                const requestMethod = response.request().method();
-                const requestUrl = response.request().url()
-
-                if (requestMethod == "POST" )
-                
-                    if(responseStatus >= 200 && responseStatus <300){
-                        try{
-                            const responseBody = await response.json();
-                            if(requestUrl === "https://ethol.pens.ac.id/api/tugas/tugas-terakhir-mahasiswa"){
-                                
-                                const status = await checkNew(responseBody);
-                                fs.writeFileSync(`${dirpath}/output.json`, JSON.stringify(responseBody, null, 2))
-
-                                switch (status){
-                                    case 0:
-                                        console.log("No New Assignment");
-                                        if(flags.assignmentRaw){
-                                            console.log(responseBody)
-                                            break;
-                                        }
-                                        const command1 = spawn("jq", [".[] | {title, deadline}", `${dirpath}/output.json`])
-                                        command1.stdout.on('data', data=>{
-                                            const jqOutput = data.toString();
-                                            console.log(jqOutput);
-                                        })
-                                        break;
-                                    case 1:
-                                        console.log("New Assignment Avaible");
-                                        if(flags.assignmentRaw){
-                                            console.log(responseBody)
-                                            break;
-                                        }
-                                        const command = spawn("jq", [".[] | {title, deadline}", `${dirpath}/output.json`])
-                                        command.stdout.on('data', data=>{
-                                            const jqOutput = data.toString();
-                                            console.log(jqOutput);
-                                        })
-                                        break;
-                                    case 2:
-                                        console.log("Error on checking file");
-                                        break;
-                                }
-
-                                //console.log(responseBody);
-                            }
-                        }
-                        catch(err){
-                        
-                        }
-                    }
+                if(flags.checkAssignment){
+                    await checkNewAssignment(response)
+                }
+                if (flags.renewMapel){
+                    await RenewMapel(response)
+                }
+                    
                 
             });
         }
@@ -206,9 +240,7 @@ async function EtholHook(){
         await page.locator("input.btn-submit").click();
         
         await sleep(2000);
-        if (flags.renewMapel){
-            await RenewMapel(page)
-        }
+        
         await page.reload();
         await sleep(3000);
 
@@ -216,12 +248,6 @@ async function EtholHook(){
         if (flags.absen){
             await Absen(page)
         }
-
-        // page.on("response", async (response)=>{
-        //     console.log(await response.json());
-        // })
-
-
 
     }
     catch(err){
@@ -267,7 +293,7 @@ async function readArg(){
 
 (async function main(){
     if(arg[2] === "--help"){
-        console.log("arg:\n  -t             Check Assignment\n  -t -raw        Output raw data of Assignment\n  -r             Update Matkuls (semester update)\n  -a {matkul}    Fill Precency in the given matkul\n  -d             Disable headless (for debugging)\n  -l             Show current matkuls\n  -l -raw        Show Matkuls information");
+        console.log("arg:\n  -t             Check Assignment\n  -t -raw        Output raw data of Assignment\n  -r             Update Matkuls (semester update)\n  -a {matkul}    Fill Precency in the given matkul\n  -d             Disable headless (for debugging)\n  -l             Show current matkuls\n  -l -raw        Show Matkuls information\nRun with -r to update matkulDetail.json file\n");
         for (let i = 0;i<arrayMatkul.length;i++){
             console.log(`matkul ${i} = ${arrayMatkul[i]}`);
         }
